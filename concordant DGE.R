c05bab968f5ec93ae890d0b4f9555da59daef79d
MikeOrman <orman.michael@gmail.com>
2022-03-29 16:12:02 -0600

    Shiny app V3

--- concordant DGE.R
+++ concordant DGE.R
@@ -12,13 +12,13 @@ concordant_DGE <- function(summary, background, mRNA){
   for (i in 1:nrow(summary)){
     concordant.DGE[i,1] <- summary$`Hugo Symbol`[i]
 #---------------------------SUBSET PATIENTS-------------------------------------
-  if (summary$Alteration[i] == "Deletion"){
-    concordant.DGE[i,2] <- "Deletion"
+  if (summary$Alteration[i] == "LOF"){
+    concordant.DGE[i,2] <- "LOF"
     altered <- subtype_subset(background, c(summary$`Hugo Symbol`[i], "loss"))
     unaltered <- subtype_subset(background, c(summary$`Hugo Symbol`[i], "WT"))
   }
-  if (summary$Alteration[i] == "Amplification"){
-    concordant.DGE[i,2] <- "Amplification"
+  if (summary$Alteration[i] == "GOF"){
+    concordant.DGE[i,2] <- "GOF"
     altered <- subtype_subset(background, c(summary$`Hugo Symbol`[i], "gain"))
     unaltered <- subtype_subset(background, c(summary$`Hugo Symbol`[i], "WT"))
   }
@@ -58,69 +58,4 @@ concordant_DGE <- function(summary, background, mRNA){
   }
   colnames(concordant.DGE) <- c("Hugo Symbol", "Alteration", "Concordant FC (altered:unaltered)", "p-val")
   return(concordant.DGE)
-}
-# Generate PDF of boxplots for concordant DGE
-# name = filname as characters
-concordant.DGE.boxplot <- function(summary, background, mRNA, name){
-  pdf(paste(name, ".pdf", sep = ""))
-  for (i in 1:nrow(summary)){
-    concordant.DGE[i,1] <- summary$`Hugo Symbol`[i]
-#---------------------------SUBSET PATIENTS-------------------------------------
-    if (summary$Alteration[i] == "Deletion"){
-      concordant.DGE[i,2] <- "Deletion"
-      altered <- subtype_subset(background, c(summary$`Hugo Symbol`[i], "loss"))
-      unaltered <- subtype_subset(background, c(summary$`Hugo Symbol`[i], "WT"))
-      altered.name <- paste(summary$`Hugo Symbol`[i], "Deleted", sep = " ")
-      unaltered.name <- paste(summary$`Hugo Symbol`[i], "WT", sep = " ")
-      }
-    if (summary$Alteration[i] == "Amplification"){
-      concordant.DGE[i,2] <- "Amplification"
-      altered <- subtype_subset(background, c(summary$`Hugo Symbol`[i], "gain"))
-      unaltered <- subtype_subset(background, c(summary$`Hugo Symbol`[i], "WT"))
-      altered.name <- paste(summary$`Hugo Symbol`[i], "Deleted", sep = " ")
-      unaltered.name <- paste(summary$`Hugo Symbol`[i], "WT", sep = " ")
-      }
-#---------------------------BOXPLOTS OF CONCORDANT DGE--------------------------
-    if (class(altered) == "data.frame" & class(unaltered) == "data.frame"){
-      altered.patients <- colnames(altered)[2:ncol(altered)]
-      unaltered.patients <- colnames(unaltered)[2:ncol(unaltered)]
-      # Subset mRNA data  
-      altered.GEX.patients <- intersect(altered.patients, colnames(mRNA)[2:ncol(mRNA)])
-      unaltered.GEX.patients <- intersect(unaltered.patients, colnames(mRNA)[2:ncol(mRNA)])
-      index <- c(TRUE)
-      for (k in 2:ncol(mRNA)){
-        if (sum(colnames(mRNA)[k] == altered.GEX.patients) == 1){
-          index[k] <- TRUE} else {index[k] <- FALSE}
-        }
-      altered.GEX <- mRNA[, index]
-      index <- c(TRUE)
-      for (k in 2:ncol(mRNA)){
-        if (sum(colnames(mRNA)[k] == unaltered.GEX.patients) == 1){
-          index[k] <- TRUE} else {index[k] <- FALSE}
-        }
-      unaltered.GEX <- mRNA[, index]
-      # Ensure at least 3 samples are present for DGE calculation
-      if (class(altered.GEX) == "data.frame" & class(unaltered.GEX) == "data.frame"){
-        if (ncol(altered.GEX) >= 4 & ncol(unaltered.GEX) >= 4) {
-          # Find index for concordant gene being tested
-          index <- which(summary$`Hugo Symbol`[i] == altered.GEX$Hugo_Symbol)[1]
-          # Altered concordant expression values
-          altered.vec <- as.numeric(altered.GEX[index,2:ncol(altered.GEX)])
-          # Unaltered concordant expression values
-          unaltered.vec <- as.numeric(unaltered.GEX[index,2:ncol(unaltered.GEX)])
-          #Combined into single data frame
-          data <- data.frame()
-          data[1:length(altered.vec),1] <- altered.name
-          data[1:length(altered.vec),2] <- altered.vec
-          data[((length(altered.vec)+1):(length(altered.vec)+length(unaltered.vec))),1] <- unaltered.name
-          data[((length(altered.vec)+1):(length(altered.vec)+length(unaltered.vec))),2] <- unaltered.vec
-          colnames(data) <- c("Genotype", "log2 Normalized Expression")
-          # Make boxplot
-          print(ggplot(data, aes(`Genotype`, `log2 Normalized Expression`, color=`Genotype`)) + 
-                  geom_boxplot() + geom_jitter() + stat_compare_means(method = "t.test"))
-      }
-    }
-    }
-  }
-  dev.off()
 }
